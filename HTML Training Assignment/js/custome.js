6567a6910fa10bb423e643e9bdba1bcf397c52ea
Vedant Patel <vedantpatel1408@gmail.com>
2023-01-31 15:10:25 +0530

    4th commit

--- HTML Training Assignment/js/custome.js
+++ HTML Training Assignment/js/custome.js
@@ -18,4 +18,21 @@ $('.toggalbtn').click(function () {
         $('#mainContent').removeClass("col-lg-10");
         $('#mainContent').addClass("col-lg-12");
     }
+    if ($(window).width() < 570) {
+
+
+        if ($('#sidebarMenu').hasClass('xyz')) {
+            $('#sidebarxyz').removeClass("d-none d-lg-block d-xl-block d-xxl-block");
+            $('#sidebarMenu').removeClass("d-none d-lg-block d-xl-block d-xxl-block");
+            $('#sidebarMenu').addClass("show d-sm-block xyz");
+        }
+        else {
+
+            $('#sidebarxyz').addClass("d-none d-lg-block d-xl-block d-xxl-block");
+            $('#sidebarMenu').addClass("d-none d-lg-block d-xl-block d-xxl-block");
+            $('#sidebarMenu').removeClass("show d-sm-block xyz");
+        }
+
+
+    }
 });
